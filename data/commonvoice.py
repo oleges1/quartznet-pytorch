dd3bece89f27db3d62e4e99ec9a40244c8635600
oleges1 <ya.hef@yandex.ru>
2020-10-15 13:59:12 +0300

    commonvoice

--- data/commonvoice.py
+++ data/commonvoice.py
@@ -11,27 +11,12 @@ class CommonVoiceDataset(torchaudio.datasets.COMMONVOICE):
         self.transforms = transforms
 
     def __getitem__(self, idx):
-        audio, sample_rate, text, _, _, _ = super().__getitem__(idx)
-        return self.transforms({'audio' : audio, 'text': text, 'sample_rate': sample_rate})
+        audio, sample_rate, dic = super().__getitem__(idx)
+        return self.transforms({'audio' : audio, 'text': dic['sentence'], 'sample_rate': sample_rate})
 
     def get_text(self, idx):
-        fileid = self._walker[idx]
-        speaker_id, chapter_id, utterance_id = fileid.split("-")
-
-        file_text = speaker_id + "-" + chapter_id + ext_txt
-        file_text = os.path.join(path, speaker_id, chapter_id, file_text)
-
-        # Load text
-        with open(file_text) as ft:
-            for line in ft:
-                fileid_text, utterance = line.strip().split(" ", 1)
-                if fileid_audio == fileid_text:
-                    break
-            else:
-                # Translation not found
-                raise FileNotFoundError("Translation not found for " + fileid_audio)
-
-        return self.transforms({'text' : utterance})['text']
+        line = self._walker[idx]
+        return self.transforms({'text' : line[2]})['text']
 
 
 def get_dataset(config, transforms=lambda x: x, part='train'):
